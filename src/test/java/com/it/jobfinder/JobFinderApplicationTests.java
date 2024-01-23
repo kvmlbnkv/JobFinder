c15a32537eff3c012876c3273f519786e00f9e53
Kamil Bienek <kamilbienek@op.pl>
2024-01-23 15:51:15 +0100

    initial commit

---
+++ src/test/java/com/it/jobfinder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
